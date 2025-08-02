be46a353c117b039dedecb204a6c572134b149c8
Андрей Евтухов <stalker10eg@mail.ru>
2025-08-02 14:56:29 +0300

    step 1 complete

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,6 @@
 import '../scss/style.scss'
 
+// Burger-menu's interactivity
 const headerButtonMenu = document.querySelector('.header__catalog-btn')
 const burgerMenu = document.querySelector('.main-menu')
 const closeBurgerMenuBtn = document.querySelector('.main-menu__close')
@@ -12,11 +13,14 @@ closeBurgerMenuBtn?.addEventListener('click', () => {
   burgerMenu?.classList.remove('main-menu--active')
 })
 
-document.addEventListener('click', (event) => {
-  const isClickInsideMenu = burgerMenu?.contains(event.target)
-  const isClickOnButton = headerButtonMenu?.contains(event.target)
+// Close burger menu on clock to any empty area
 
-  if (!isClickInsideMenu && !isClickOnButton) {
-    burgerMenu?.classList.remove('main-menu--active')
-  }
-})
+// document.addEventListener('click', (event) => {
+//   const target = event.target as HTMLElement;
+//   const isInsideContent = document.querySelector('.main-menu__wrapper')?.contains(target)
+//   const isClickOnButton = headerButtonMenu?.contains(target)
+//
+//   if (!isInsideContent && !isClickOnButton) {
+//     burgerMenu?.classList.remove('main-menu--active')
+//   }
+// })
